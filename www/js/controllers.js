e8da91b8b532ce68bc602d04df159df3e2516228
Julian Biebelberg <jbiebelberg@gmail.com>
2016-11-05 18:24:49 -0400

    added android platform, testing new configuration

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,10 @@
-angular.module('starter.controllers', ['ngCordova'])
- 
-.controller('VideoCtrl', function($scope, $cordovaCapture, VideoService) {
-    // TBD
-    $scope.clip = '';
- 
-	$scope.captureVideo = function() {
-		$cordovaCapture.captureVideo().then(function(videoData) {
-			VideoService.saveVideo(videoData).success(function(data) {
-				$scope.clip = data;
-				$scope.$apply();
-			}).error(function(data) {
-				console.log('ERROR: ' + data);
-			});
-		});
-	};
+angular.module('app.controllers', [])
+  
+.controller('pageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
 
-	$scope.urlForClipThumb = function(clipUrl) {
-	var name = clipUrl.substr(clipUrl.lastIndexOf('/') + 1);
-	var trueOrigin = cordova.file.dataDirectory + name;
-	var sliced = trueOrigin.slice(0, -4);
-	return sliced + '.png';
-	}
+
+}])
  
-	$scope.showClip = function(clip) {
-		console.log('show clip: ' + clip);
-	}
-});

42f0c446c48c684a021eced7506b5850ac28e789
Julian Biebelberg <jbiebelberg@gmail.com>
2016-11-05 19:25:03 -0400

    added video logic

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,6 +5,25 @@ angular.module('app.controllers', [])
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
+	$scope.video = function(){
+		// start video capture
+		navigator.device.capture.captureVideo(captureSuccess, captureError, {limit:2});
 
+		// capture error callback
+		var captureError = function(error) {
+		    navigator.notification.alert('Error code: ' + error.code, null, 'Capture Error');
+		};
+
+		// capture callback
+		var captureSuccess = function(mediaFiles) {
+		    var i, path, len;
+		    for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+		        path = mediaFiles[i].fullPath;
+		        // do something interesting with the file
+		        console.log(path);
+		    }
+		};
+
+	}
 }])
  
